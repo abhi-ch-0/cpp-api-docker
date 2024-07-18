6e53e8b001c5c83a29c8247c9e4b25c0748ce11b
“abhi” <abhichouhan@proton.me>
2024-07-18 23:11:54 +0530

    create tcp socket

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int tcp_socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (tcp_socket_fd == -1)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    close(tcp_socket_fd);
     return 0;
 }
