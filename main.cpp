99c4fd0c59886218c7430d2e74695faf13ebad67
“abhi” <abhichouhan@proton.me>
2024-07-18 23:18:53 +0530

    close socket on interrupt

--- main.cpp
+++ main.cpp
@@ -2,15 +2,32 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <signal.h>
+
+int tcp_socket_fd = -1;
+
+void signalHandler(int signum)
+{
+    std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
+    if (tcp_socket_fd != -1)
+    {
+        std::cout << "Closing the socket." << std::endl;
+        close(tcp_socket_fd);
+    }
+    exit(signum);
+}
 
 int main()
 {
+    signal(SIGINT, signalHandler);
+
     int tcp_socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (tcp_socket_fd == -1)
     {
-        perror("socket creation failed");
+        perror("Socket creation failed.");
         exit(EXIT_FAILURE);
     }
+    std::cout << "Socket created successfully." << std::endl;
     close(tcp_socket_fd);
     return 0;
 }
